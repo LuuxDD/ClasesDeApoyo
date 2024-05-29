a394dff1bbe3641164e57f6a4764bab9c1c0043e
LeandroLJ <leandrolazzaroni@live.com>
2024-05-29 19:04:02 -0300

    Agregar archivos de proyecto.

---
+++ ClasesDeApoyo/Clases/Ventilador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesDeApoyo.Clases
+{
+     public class Ventilador
+    
+    {
+        //CAMPOS
+        private string Color;
+        private string Marca;
+        private bool Encendido;
+
+        //Propiedades 
+        public int Velocidad { get; set; }
+        public int Movimiento { get; set; }
+
+        public Ventilador(string color, string marca)
+        {
+            Color = color;
+            Marca = marca;
+        }
+        public void Encender()
+        {
+            this.Encendido = true;
+        }
+      
+    }
+      
+    
+
+}
